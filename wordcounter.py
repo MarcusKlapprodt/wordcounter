46d8dd715a953b35ef2ff4b75a3e7ab5791280ab
deimos <taus_i_melek@yahoo.de>
2019-02-18 19:57:51 +0100

    created the wordcounter in python and added a textfile

--- wordcounter.py
+++ wordcounter.py
@@ -1,1 +1,19 @@
+import queue
+with open("textfile.txt", "r") as textfile:
+    data = textfile.read()
+
+d = {}
+for word in data.split(" "):
+    if word in d:
+        d[word]=d[word] +1
+    else:
+        d[word]=1
+print(d)
+
+pq = queue.PriorityQueue()
+
+for word, number in d.items():
+    pq.put((-number, word))
+for i in range(0,5):
+    print(pq.get())
 
