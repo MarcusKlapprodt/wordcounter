332e679ab08c1a4d15ed7de870262185d4d14460
deimos <taus_i_melek@yahoo.de>
2019-02-18 20:00:32 +0100

    added documentation

--- wordcounter.py
+++ wordcounter.py
@@ -2,18 +2,21 @@ import queue
 with open("textfile.txt", "r") as textfile:
     data = textfile.read()
 
+# create a dictionary and put in single words
 d = {}
 for word in data.split(" "):
     if word in d:
         d[word]=d[word] +1
     else:
         d[word]=1
-print(d)
 
+# creating a Priority Queue and adding the dictionary to the queue
 pq = queue.PriorityQueue()
 
 for word, number in d.items():
     pq.put((-number, word))
+
+#printing the result of the 5 most common words
 for i in range(0,5):
     print(pq.get())
 
