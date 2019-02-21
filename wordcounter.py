449e059f34d53a112613effeca4509bf497e95e0
deimos <taus_i_melek@yahoo.de>
2019-02-21 14:31:39 +0100

    Added the first part of the pdf creator and wrote the first line to the pdf "simple_demo.pdf"

--- wordcounter.py
+++ wordcounter.py
@@ -10,6 +10,9 @@ for word in data.split(" "):
     else:
         d[word]=1
 
+DictLen = "The total number of different words is " + str(len(d))
+
+
 # creating a Priority Queue and adding the dictionary to the queue
 pq = queue.PriorityQueue()
 
@@ -23,7 +26,17 @@ for i in range(0,5):
 ## Export the outpunt in a pdf file
 
 
+## create pdf
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=32)
+
 ## show the total number of words
+pdf.cell(200, 200, txt=DictLen, ln=1, align="C")
+pdf.output("simple_demo.pdf")
+
 
 ## show the total number of characters
 
