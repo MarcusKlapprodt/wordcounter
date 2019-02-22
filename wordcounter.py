fb9d4c7ec56cea827b99ae2e534b23a729db75bd
deimos <taus_i_melek@yahoo.de>
2019-02-22 13:53:59 +0100

    created the wordcounter in python and added a textfile

--- wordcounter.py
+++ wordcounter.py
@@ -2,6 +2,15 @@ import queue
 with open("textfile.txt", "r") as textfile:
     data = textfile.read()
 
+# count the total number of words
+word_count = 0
+for word in data.split(" "):
+    word_count += 1
+
+print(word_count)
+word_count_text = "The total number of words is " + str(word_count)
+
+
 # create a dictionary and put in single words
 d = {}
 for word in data.split(" "):
@@ -31,15 +40,15 @@ from fpdf import FPDF
 
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font("Arial", size=32)
+pdf.set_font("Arial", size=16)
 
 ## show the total number of words
+pdf.cell(200, 100, txt=word_count_text, ln=1, align="C")
+
+## show the number of individual words
 pdf.cell(200, 200, txt=DictLen, ln=1, align="C")
-pdf.output("simple_demo.pdf")
-
-
-## show the total number of characters
 
 ## show the top 20 words used and their count
 
-
+## output the pdf
+pdf.output("simple_demo.pdf")
